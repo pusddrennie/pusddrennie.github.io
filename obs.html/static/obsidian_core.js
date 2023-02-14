ec60ce97cf24005edb983eefd59276c60ecdab87
David E Rennie <rennie.david@pusd.us>
2023-02-14 10:22:04 -0800

    Fixed typos and URL issues in block features like footnotes and callouts.

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '313392420094427779773630994792957653'                       // used to check whether the localStorage data is stale
+var gzip_hash = '258317444851366020737108742780650069343'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
