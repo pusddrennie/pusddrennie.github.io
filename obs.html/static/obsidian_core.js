21855e306876c3340a213c2d451eae3cafce1ac2
David E Rennie <rennie.david@pusd.us>
2023-02-28 14:18:51 -0800

    Started initial placeholders for strategies and resources

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '287807268611547994521862630394942917030'                       // used to check whether the localStorage data is stale
+var gzip_hash = '33088850597354015639224581428143675345'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
