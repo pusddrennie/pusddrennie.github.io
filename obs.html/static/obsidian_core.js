55bac1bb0de8b56ce7e5c45967d0c062df8b2a5d
David E Rennie <rennie.david@pusd.us>
2023-02-28 13:13:05 -0800

    Reorganized files into folders; began development of initial maps

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '11568045226968079455350820328315447697'                       // used to check whether the localStorage data is stale
+var gzip_hash = '287807268611547994521862630394942917030'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
