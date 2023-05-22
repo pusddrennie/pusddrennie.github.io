4f690cbd3991f1503b4dbf744e96173eb72393e3
David Rennie <rennie.david@pusd.us>
2023-05-22 14:18:31 -0700

    Updated Navigation Link in Top Menu bar

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '100582061360840103499748869068937437903'                       // used to check whether the localStorage data is stale
+var gzip_hash = '126357318468186146222287944087918461599'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
